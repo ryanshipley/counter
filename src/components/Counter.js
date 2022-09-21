cab277b25ab80f7989ebbe82ba6e51746fb9511a
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-21 11:01:08 -0400

    adds useState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Counter(){
-    return(
-    <>
-    <span>Current Count: 0</span>
-    <section>
-        <button>+</button>
-        <button>-</button>
-    </section>
-    </>
+    // const [count, setCount] = useState(0);
+    // const [name, setName] = useState("Gender Bender");
+    // const [qual, setQual] = useState("CSS");
 
-    );
+    //array deconstructor
+    const [count, setCount]= useState(0);
+
+
+
+    return (
+        <div>
+          <span>Current Count: {count}</span>
+          <section>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+          </section>
+        </div>
+      );
 };
 
 export default Counter;
