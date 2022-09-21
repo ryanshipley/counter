ba0a2e84bef7606155902162a8cf5d54aade305b
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-21 08:44:40 -0400

    creates counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Counter(){
+    return(
+    <>
+    <span>Current Count: 0</span>
+    <section>
+        <button>+</button>
+        <button>-</button>
+    </section>
+    </>
+
+    );
+};
+
+export default Counter;
